bbd4107978a2033e35fc9ef709eede064956929e
Meryem Essalhi <meryem.essalhi@gmail.com>
2024-11-11 15:06:51 +0000

    JS weather object

---
+++ weather.js
@@ -0,0 +1,10 @@
+// let weather ={
+//     london: 10,
+//     cardiff : 22,
+//     Dublin: 12,
+//     Birmingham: 12,
+//      getWeather(location){
+//         return `Temperature in  ${location} is ${this[location]}`;
+//     }
+// }
+// console.log(weather.getWeather('Birmingham'));
